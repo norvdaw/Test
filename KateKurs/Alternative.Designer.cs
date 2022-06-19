b41a182c09872d452a154d5421bf8e5fa18a2a18
norvdaw <e.p.zorikhina@gmail.com>
2022-06-19 18:08:04 +0300

    Test

---
+++ KateKurs/Alternative.Designer.cs
@@ -0,0 +1,306 @@
+﻿
+namespace KateKurs
+{
+    partial class Alternative
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Alternative));
+            this.dgvAlt = new System.Windows.Forms.DataGridView();
+            this.dataSet1 = new KateKurs.DataSet1();
+            this.bindingNavigator1 = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnNorm = new System.Windows.Forms.Button();
+            this.btnUch = new System.Windows.Forms.Button();
+            this.btnSos = new System.Windows.Forms.Button();
+            this.normBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.normTableAdapter = new KateKurs.DataSet1TableAdapters.normTableAdapter();
+            this.sostavBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sostavTableAdapter = new KateKurs.DataSet1TableAdapters.sostavTableAdapter();
+            this.uchetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.uchetTableAdapter = new KateKurs.DataSet1TableAdapters.uchetTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
+            this.bindingNavigator1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.normBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sostavBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uchetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAlt
+            // 
+            this.dgvAlt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAlt.Location = new System.Drawing.Point(41, 30);
+            this.dgvAlt.Name = "dgvAlt";
+            this.dgvAlt.RowHeadersWidth = 51;
+            this.dgvAlt.RowTemplate.Height = 24;
+            this.dgvAlt.Size = new System.Drawing.Size(1258, 392);
+            this.dgvAlt.TabIndex = 0;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bindingNavigator1
+            // 
+            this.bindingNavigator1.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.bindingNavigator1.CountItem = this.bindingNavigatorCountItem;
+            this.bindingNavigator1.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.bindingNavigator1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.bindingNavigator1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem});
+            this.bindingNavigator1.Location = new System.Drawing.Point(0, 0);
+            this.bindingNavigator1.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.bindingNavigator1.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.bindingNavigator1.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.bindingNavigator1.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.bindingNavigator1.Name = "bindingNavigator1";
+            this.bindingNavigator1.PositionItem = this.bindingNavigatorPositionItem;
+            this.bindingNavigator1.Size = new System.Drawing.Size(1397, 27);
+            this.bindingNavigator1.TabIndex = 1;
+            this.bindingNavigator1.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorAddNewItem.Text = "Добавить";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(55, 24);
+            this.bindingNavigatorCountItem.Text = "для {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Общее число элементов";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorDeleteItem.Text = "Удалить";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveFirstItem.Text = "Переместить в начало";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMovePreviousItem.Text = "Переместить назад";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 27);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Положение";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 27);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Текущее положение";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveNextItem.Text = "Переместить вперед";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveLastItem.Text = "Переместить в конец";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 27);
+            // 
+            // btnNorm
+            // 
+            this.btnNorm.Location = new System.Drawing.Point(350, 507);
+            this.btnNorm.Name = "btnNorm";
+            this.btnNorm.Size = new System.Drawing.Size(168, 48);
+            this.btnNorm.TabIndex = 2;
+            this.btnNorm.Text = "Нормы затрат труда";
+            this.btnNorm.UseVisualStyleBackColor = true;
+            this.btnNorm.Click += new System.EventHandler(this.btnNorm_Click);
+            // 
+            // btnUch
+            // 
+            this.btnUch.Location = new System.Drawing.Point(595, 507);
+            this.btnUch.Name = "btnUch";
+            this.btnUch.Size = new System.Drawing.Size(168, 48);
+            this.btnUch.TabIndex = 3;
+            this.btnUch.Text = "Учет выработки";
+            this.btnUch.UseVisualStyleBackColor = true;
+            this.btnUch.Click += new System.EventHandler(this.btnUch_Click);
+            // 
+            // btnSos
+            // 
+            this.btnSos.Location = new System.Drawing.Point(844, 507);
+            this.btnSos.Name = "btnSos";
+            this.btnSos.Size = new System.Drawing.Size(168, 48);
+            this.btnSos.TabIndex = 4;
+            this.btnSos.Text = "Личный состав";
+            this.btnSos.UseVisualStyleBackColor = true;
+            this.btnSos.Click += new System.EventHandler(this.btnSos_Click);
+            // 
+            // normBindingSource
+            // 
+            this.normBindingSource.DataMember = "norm";
+            this.normBindingSource.DataSource = this.dataSet1;
+            // 
+            // normTableAdapter
+            // 
+            this.normTableAdapter.ClearBeforeFill = true;
+            // 
+            // sostavBindingSource
+            // 
+            this.sostavBindingSource.DataMember = "sostav";
+            this.sostavBindingSource.DataSource = this.dataSet1;
+            // 
+            // sostavTableAdapter
+            // 
+            this.sostavTableAdapter.ClearBeforeFill = true;
+            // 
+            // uchetBindingSource
+            // 
+            this.uchetBindingSource.DataMember = "uchet";
+            this.uchetBindingSource.DataSource = this.dataSet1;
+            // 
+            // uchetTableAdapter
+            // 
+            this.uchetTableAdapter.ClearBeforeFill = true;
+            // 
+            // Alternative
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1397, 580);
+            this.Controls.Add(this.btnSos);
+            this.Controls.Add(this.btnUch);
+            this.Controls.Add(this.btnNorm);
+            this.Controls.Add(this.bindingNavigator1);
+            this.Controls.Add(this.dgvAlt);
+            this.Name = "Alternative";
+            this.Text = "Alternativ";
+            this.Load += new System.EventHandler(this.Alternative_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).EndInit();
+            this.bindingNavigator1.ResumeLayout(false);
+            this.bindingNavigator1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.normBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sostavBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uchetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAlt;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingNavigator bindingNavigator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.Button btnNorm;
+        private System.Windows.Forms.Button btnUch;
+        private System.Windows.Forms.Button btnSos;
+        
+        private System.Windows.Forms.BindingSource normBindingSource;
+        private DataSet1TableAdapters.normTableAdapter normTableAdapter;
+        private System.Windows.Forms.BindingSource sostavBindingSource;
+        private DataSet1TableAdapters.sostavTableAdapter sostavTableAdapter;
+        private System.Windows.Forms.BindingSource uchetBindingSource;
+        private DataSet1TableAdapters.uchetTableAdapter uchetTableAdapter;
+    }
+}
---
+++ KateKurs/DataSet1.cs
@@ -0,0 +1,10 @@
+﻿namespace KateKurs
+{
+    partial class DataSet1
+    {
+        partial class Zadacha3DataTable
+        {
+        }
+    }
+}
+
